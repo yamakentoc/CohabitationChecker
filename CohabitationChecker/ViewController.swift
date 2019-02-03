38dcc7f378d015c231ee83082bb520f737b67ae3
yamakentoc <yamakentoc@gmail.com>
2019-02-04 06:58:00 +0900

    Initial Commit

---
+++ CohabitationChecker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CohabitationChecker
+//
+//  Created by 山口賢登 on 2019/02/04.
+//  Copyright © 2019 山口賢登. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
