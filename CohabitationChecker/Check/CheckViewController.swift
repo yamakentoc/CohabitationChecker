f8609051c52a11fa1f248ad1bab7460f8c744343
yamakentoc <yamakentoc@gmail.com>
2019-02-09 18:10:42 +0900

    nextButtonを追加

--- CohabitationChecker/Check/CheckViewController.swift
+++ CohabitationChecker/Check/CheckViewController.swift
@@ -12,22 +12,35 @@ import Koloda
 class CheckViewController: UIViewController {
 
     @IBOutlet weak var kolodaView: KolodaView!
+    @IBOutlet weak var nextButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
     var itemText:[String] = ["休日の過ごし方", "トイレットペーパー", "バスタオルを使う頻度"]
     var firstAnswer: [String] = ["外に出かける", "シングル", "毎日替える"]
     var secondAnswer: [String] = ["家で過ごす", "ダブル", "何日か使ってから"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        initKolodaView()
+        kolodaView.dataSource = self
+        kolodaView.delegate = self
+        initButton(nextButton)
+        initButton(backButton)
     }
     
-    @IBAction func tapUndoButton(_ sender: UIButton) {
+    @IBAction func tapNextButton(_ sender: UIButton) {
+        self.kolodaView.swipe(.up)
+    }
+    
+    @IBAction func tapBackButton(_ sender: UIButton) {
         self.kolodaView.revertAction()
     }
     
-    func initKolodaView() {
-        kolodaView.dataSource = self
-        kolodaView.delegate = self
+    func initButton(_ button: UIButton) {
+        button.layer.cornerRadius = backButton.bounds.width / 2
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.3//影の濃さ
+        button.layer.shadowRadius = 4//ぼかし
+        button.layer.shadowOffset = CGSize(width: 0, height: 1.5)
     }
 
 }
